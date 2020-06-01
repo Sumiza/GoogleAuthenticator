012edd00b57930688bd96c0179aa9e16719e92ba
Sumiza <64884509+Sumiza@users.noreply.github.com>
2020-06-01 02:21:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 yum install -y google-authenticator
 google-authenticator -t -d -f --rate-limit=3 --rate-time=30 --window-size=3
 echo auth required pam_google_authenticator.so >> /etc/pam.d/sshd
+(cat < /etc/pam.d/sshd | grep 'auth required pam_google_authenticator.so') || (echo auth required pam_google_authenticator.so >> /etc/pam.d/sshd)
 sed -i '/ChallengeResponseAuthentication yes/c\ChallengeResponseAuthentication yes' /etc/ssh/sshd_config
 sed -i '/ChallengeResponseAuthentication no/c\#ChallengeResponseAuthentication no' /etc/ssh/sshd_config
 systemctl restart sshd
