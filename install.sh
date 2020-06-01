bba8e4d4895d8d0a947c21d5fd7945a9deaf7165
Sumiza <64884509+Sumiza@users.noreply.github.com>
2020-06-01 01:26:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ echo auth required pam_google_authenticator.so >> /etc/pam.d/sshd
 sed -i '/ChallengeResponseAuthentication yes/c\ChallengeResponseAuthentication yes' /etc/ssh/sshd_config
 sed -i '/ChallengeResponseAuthentication no/c\#ChallengeResponseAuthentication no' /etc/ssh/sshd_config
 systemctl restart sshd
-echo "----------DONE---------------"
+echo "-Please copy down the emergency codes and test login with another terminal window before logging out-"
